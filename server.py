a1c226f66e53474d3f7769887f741d8f36e17ed2
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-02-21 13:46:35 -0600

    Updated server.py: add_user()

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from user import User
 app = Flask(__name__)
 app.secret_key = " 8891techa0128"
@@ -7,5 +7,18 @@ app.secret_key = " 8891techa0128"
 def index():
     return render_template("index.html", users = User.get_all())
 
+@app.route("/add-user", methods = ["GET", "post"])
+def add_user():
+    if request.method == "GET":
+        return render_template("new_user.html")
+    else:
+        data = {
+            "first_name": request.form["first_name"],
+            "last_name": request.form["last_name"],
+            "email": request.form["email"]
+        }
+        User.save(data)
+        return redirect("/")
+    
 if __name__ == "__main__":
     app.run(debug=True)
