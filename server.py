04f678b411363b9bb2a3b264c4e094d128a61c4b
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-02-21 13:19:23 -0600

    Display all users from the database on the Get Users page, and updated README.md(added project instructions)

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-# from friend import Friend
+from user import User
 app = Flask(__name__)
 app.secret_key = " 8891techa0128"
 
 @app.route("/")
 def index():
-    return render_template("index.tml")
+    return render_template("index.html", users = User.get_all())
 
 if __name__ == "__main__":
     app.run(debug=True)
